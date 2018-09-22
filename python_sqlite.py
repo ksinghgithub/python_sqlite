460b365c08501c3d0c364ede35dafaa9eeb3cc0b
ksinghgithub <43468601+ksinghgithub@users.noreply.github.com>
2018-09-22 00:58:45 -0700

    slice/del list + optimizations

--- python_sqlite.py
+++ python_sqlite.py
@@ -9,6 +9,8 @@ import queue as queue
 from concurrent.futures import *
 import time
 from datetime import datetime
+from toolz import partition_all
+
 
 
 class python_sqlite:
@@ -23,7 +25,7 @@ class python_sqlite:
         # self.db.execute('pragma journal_mode=memory')
         # self.db.execute('pragma journal_mode=wal')
         self.db.execute('pragma SYNCHRONOUS=1')
-        self.db.execute('pragma PAGE_SIZE=65535')
+        self.db.execute('pragma PAGE_SIZE=4096')
         self.db.execute('pragma cache_size = 8192')
         self.db.execute('DROP table if exists blacklist')
         self.db.execute('CREATE TABLE blacklist (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, url TEXT NOT NULL UNIQUE)')
@@ -70,12 +72,39 @@ class python_sqlite:
             if not urls:
                 break
 
-            templist = []
-            i = 0
-            while i < self.bulk_insert_entries and len(urls) > 0:
-                templist.append(urls.pop())
-                i += 1
+            templist = urls[-self.bulk_insert_entries:]
+            del urls[-self.bulk_insert_entries:]
+            try:
+                start_commit = datetime.now()
+                # self.cursor.execute('begin')
+                self.cursor.executemany('''INSERT OR IGNORE INTO blacklist(url) VALUES(:url)''', (templist))
+                # self.cursor.execute('commit')
+                end_commit = datetime.now() - start_commit
+                print('add_blacklist_url:: total time for INSERT OR IGNORE INTO blacklist {} entries = {}'.format(len(templist), end_commit))
+            except sqlite3.Error as e:
+                # self.cursor('rollback')
+                print("add_blacklist_url:: Database error: %s" % e)
+            except Exception as e:
+                # self.cursor('rollback')
+                print("add_blacklist_url:: Exception in _query: %s" % e)
+        self.db.commit()
+        time_elapsed = datetime.now() - start_time
+        print('add_blacklist_url:: total time for {} entries = {}'.format(records, time_elapsed))
+        # self.db.execute('pragma auto_vacuum=1')
 
+
+
+    def add_blacklist_url_DanD(self, urls):
+        # print('add_blacklist_url:: entries = {}'.format(len(urls)))
+        start_time = datetime.now()
+        records = len(urls)
+        self.cursor.execute('begin')
+        while True:
+            if not urls:
+                break
+
+            templist = urls[-self.bulk_insert_entries:]
+            del urls[-self.bulk_insert_entries:]
             try:
                 start_commit = datetime.now()
                 self.cursor.executemany('''INSERT OR IGNORE INTO blacklist(url) VALUES(:url)''', (templist))
@@ -84,10 +113,33 @@ class python_sqlite:
             except sqlite3.Error as e:
                 print("add_blacklist_url:: Database error: %s" % e)
             except Exception as e:
-                    print("add_blacklist_url:: Exception in _query: %s" % e)
+                print("add_blacklist_url:: Exception in _query: %s" % e)
+        self.cursor.execute('commit')
         self.db.commit()
         time_elapsed = datetime.now() - start_time
         print('add_blacklist_url:: total time for {} entries = {}'.format(records, time_elapsed))
+
+
+
+    def add_blacklist_url_Javier(self, urls):
+        records = len(urls)
+        # print('add_blacklist_url:: entries = {}'.format(len(urls)))
+        start_time = datetime.now()
+        for batch in partition_all(self.bulk_insert_entries, urls):
+            try:
+                start_commit = datetime.now()
+                self.cursor.executemany('''INSERT OR IGNORE INTO blacklist(url) VALUES(:url)''', batch)
+                end_commit = datetime.now() - start_commit
+                print('add_blacklist_url:: total time for INSERT OR IGNORE INTO blacklist {} entries = {}'.format(
+                    len(batch), end_commit))
+            except sqlite3.Error as e:
+                print("add_blacklist_url:: Database error: %s" % e)
+            except Exception as e:
+                print("add_blacklist_url:: Exception in _query: %s" % e)
+            self.db.commit()
+        time_elapsed = datetime.now() - start_time
+        print('add_blacklist_url:: total time for {} entries = {}'.format(records, time_elapsed))
+
 
 
 pysql = python_sqlite()
